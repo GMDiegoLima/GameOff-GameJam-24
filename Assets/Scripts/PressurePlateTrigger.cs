5ca1aef507c4ce65ad21bd524d2b7b5e34b0c951
Diego Lima <gmdiegolima@hotmail.com>
2024-11-06 17:30:03 -0300

    add goblin animations and grass tileset

--- Assets/Scripts/PressurePlateTrigger.cs
+++ Assets/Scripts/PressurePlateTrigger.cs
@@ -5,16 +5,19 @@ public class PressurePlateTrigger : MonoBehaviour
 {
     public UnityEvent onActivate;
     public UnityEvent onDeactivate;
+    public GameObject player;
+    PlayerController playerScript;
     Animator animator;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        playerScript = player.GetComponent<PlayerController>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") || other.CompareTag("PressureTrigger"))
+        if (other.CompareTag("Player") && !playerScript.flying || other.CompareTag("PressureTrigger"))
         {
             animator.SetBool("Activated", true);
             onActivate?.Invoke();
@@ -23,7 +26,7 @@ public class PressurePlateTrigger : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player") || other.CompareTag("PressureTrigger"))
+        if (other.CompareTag("Player") && !playerScript.flying || other.CompareTag("PressureTrigger"))
         {
             animator.SetBool("Activated", false);
             onDeactivate?.Invoke();
