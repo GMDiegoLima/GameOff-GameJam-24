198929a2f3e95b0c1bcb0f5b19e89ce195072eaa
Santiago Barragan <94990891+Snati1206@users.noreply.github.com>
2024-11-19 21:15:19 +0100

    menu rtcps added
    Music
    Master
    SFX

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -21,7 +21,6 @@ namespace AK
         static const AkUniqueID MENU_CURSOR_SELECT = 3200708208U;
         static const AkUniqueID NORMAL_DIALOGUE = 365141489U;
         static const AkUniqueID PLATE = 282449107U;
-        static const AkUniqueID ROCK_PUSH = 2827948781U;
         static const AkUniqueID TESTINGEVENT = 946158365U;
     } // namespace EVENTS
 
@@ -83,6 +82,13 @@ namespace AK
 
     } // namespace SWITCHES
 
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID MASTER = 4056684167U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace GAME_PARAMETERS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
@@ -102,6 +108,7 @@ namespace AK
         static const AkUniqueID NPCMASTER = 2033911932U;
         static const AkUniqueID OBJECTSMASTER = 3123271961U;
         static const AkUniqueID PLAYERMASTER = 3538689948U;
+        static const AkUniqueID SFXMASTER = 1681633080U;
         static const AkUniqueID UIMASTER = 1992125169U;
     } // namespace BUSSES
 
